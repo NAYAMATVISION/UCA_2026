e2a812618c243a30119f7a7d995d46e83d934758
NAYAMATVISION <nayamatemeet@gmail.com>
2026-07-26 10:40:40 +0530

    added code for comment removal

---
+++ UCA_week3/programming/q4.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+
+int main()
+{
+    int ch, next;
+
+    while ((ch = getchar()) != EOF)
+    {
+        if (ch == '"')
+        {
+            putchar(ch);
+
+            while ((ch = getchar()) != EOF)
+            {
+                putchar(ch);
+
+                if (ch == '\\')
+                {
+                    ch = getchar();
+                    if (ch != EOF)
+                        putchar(ch);
+                }
+                else if (ch == '"')
+                {
+                    break;
+                }
+            }
+        }
+        else if (ch == '\'')
+        {
+            putchar(ch);
+
+            while ((ch = getchar()) != EOF)
+            {
+                putchar(ch);
+
+                if (ch == '\\')
+                {
+                    ch = getchar();
+                    if (ch != EOF)
+                        putchar(ch);
+                }
+                else if (ch == '\'')
+                {
+                    break;
+                }
+            }
+        }
+        else if (ch == '/')
+        {
+            next = getchar();
+
+            if (next == '/')
+            {
+                while ((ch = getchar()) != EOF && ch != '\n');
+
+                if (ch == '\n')
+                    putchar('\n');
+            }
+            else if (next == '*')
+            {
+                int prev = 0;
+
+                while ((ch = getchar()) != EOF)
+                {
+                    if (prev == '*' && ch == '/')
+                        break;
+
+                    prev = ch;
+                }
+            }
+            else
+            {
+                putchar(ch);
+
+                if (next != EOF)
+                    ungetc(next, stdin);
+            }
+        }
+        else
+        {
+            putchar(ch);
+        }
+    }
+
+    return 0;
+}
---
+++ UCA_week3/programming/sample.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello"); // This is a comment
+
+    /* Multi-line
+       comment */
+
+    printf("Bye");
+
+    return 0;
+}
